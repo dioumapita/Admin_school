0a569b1cbabba8a97c9db43ab4cc2dab2906d46a
dioumapita <62258430+dioumapita@users.noreply.github.com>
2020-09-11 20:59:47 +0000

    Delete eleves_mode_grille.blade.php

--- resources/views/pages/eleves/eleves_mode_grille.blade.php
+++
@@ -1,5 +0,0 @@
-    {{-- on herite du theme activer --}}
-        @extends($chemin_theme_actif,['title' =>'eleves-mode-grille'])
-            @section('content')
-                @livewire('liste-eleve-mode-grille')
-            @endsection
